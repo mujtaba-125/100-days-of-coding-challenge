c760043210ca65a1bde1aacdc1fa143d67008b6d
Mujtaba Rasheed <mr.muju139@gmail.com>
2024-03-22 12:49:38 +0500

    The day 1 challenge

---
+++ day 1/question 2/q2.ts
@@ -0,0 +1,10 @@
+/* Personal Message: Store a person’s name in a variable and print a message to
+them, like “Hello Asharib, would you like to learn some TypeScript today?”*/
+
+//create a variable to store a name
+
+let Person_name:string="Mujtaba Rasheed Malik"
+
+//print the name store in variable
+
+console.log(" “Hello ," +Person_name+" would you like to learn some TypeScript today?")
